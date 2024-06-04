548f17cd0ebdbb02f9a7592458e96e1195e6dc56
techcoder-04 <78724363+techcoder-04@users.noreply.github.com>
2024-06-04 01:23:35 +0000

    h5

--- app/src/main/java/org/oppia/android/app/home/HomeActivity.kt
+++ app/src/main/java/org/oppia/android/app/home/HomeActivity.kt
@@ -22,7 +22,7 @@ import org.oppia.android.app.translation.AppLanguageResourceHandler
 import org.oppia.android.util.logging.CurrentAppScreenNameIntentDecorator.decorateWithScreenName
 import javax.inject.Inject
 
-// Home Activity test 4
+// Home Activity test 5
 /** The central activity for all users entering the app. */
 class HomeActivity :
   InjectableAutoLocalizedAppCompatActivity(),
